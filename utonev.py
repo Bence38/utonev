c48d83d8b79ff9ac69cc0980ecfc7eefb0539555
Rbencuska69 <sutkabence2006@gmail.com>
2024-04-12 13:57:18 +0200

    nő férfi és népesség adatok

--- utonev.py
+++ utonev.py
@@ -10,6 +10,9 @@ class Nev:
     def __str__(self):
         return f'Utónév: {self.nev}, Neme: {self.nem}'
 utonevek = []
+ferfi = 0
+no = 0
+db = 0
 f = open('UTONEV.TXT', 'rt' , encoding = 'ansi')
 f.readline()
 
@@ -18,3 +21,21 @@ for sor in f :
     utonevek.append(Nev(tmp[0], tmp[1], tmp[2], tmp[3],tmp[4], tmp[5]))
 for utonev in utonevek:
     print(utonev)
+
+ffo = 0
+for ferfi in utonevek:
+    if ferfi.nem == 'F':
+        if ferfi.elso != '':
+            ffo += int(ferfi.elso)
+print(f"{ffo} férfi volt")
+
+nfo = 0
+for no in utonevek:
+    if no.nem == 'N':
+        if no.elso != '':
+            nfo += int(no.elso)
+print(f"{nfo} nő volt")
+
+print(f"{ffo + nfo} volt a népesség száma")
+
+print(f"{db} utónévről van adat")
